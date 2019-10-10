c6baefde08d5595748346f61437600f748c96388
mshanemc <shane.mclaughlin@salesforce.com>
2019-10-10 16:55:29 -0500

    installs package successfully

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,3 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:package:install --package 04t3i000001gxE7AAI
+sfdx force:package:install --package 04t3i000001gxE7AAI -w 20
 sfdx force:org:open
